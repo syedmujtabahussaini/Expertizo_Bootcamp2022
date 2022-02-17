fced8ff5b5b58ea96fa06fb91d1eacdc03b61898
Syed Mujtaba Hussaini <mujtabac1@gmail.com>
2022-02-17 19:07:15 +0500

    17-feb

--- src/LeftPanelButtons.js
+++ src/LeftPanelButtons.js
@@ -1,43 +1,72 @@
 function LeftPanelButtons() {
   return (
     <div className=" w-[85%]">
-      <button className=" py-1 flex justify-start align-middle bg-blue-700 w-full rounded-t-lg rounded-b-lg h-10 text-white  my-3">
-        <img src="Home.svg" alt="" width={30} className=" mx-4 inline " />
-        <span className="py-2 flex align-middle text-sm">Home</span>
-      </button>
-      <button className=" py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="book.svg" alt="" width={30} className="mx-4 inline " />
-        <span className="py-2 flex align-middle text-sm">myBook</span>
-      </button>
-
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="video.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myVid</span>
+      <button className=" shadow-sm hover:bg-[#084fe5] hover:text-white  flex justify-start  py-1  bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-11 text-black  my-3">
+        <img
+          src="Home.svg"
+          alt=""
+          width={20}
+          className=" mx-4 my-auto inline "
+        />
+        <span className="  hover:text-white flex my-auto py-2  text-sm font-bold ">
+          Home
+        </span>
       </button>
 
-      <button className=" py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="idea.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myIdea</span>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img
+          src="book.svg"
+          alt=""
+          width={20}
+          className="mx-4 inline my-auto "
+        />
+        <span className="py-2 flex align-middle text-sm font-bold my-auto">
+          myBook
+        </span>
       </button>
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="job.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myjob</span>
+
+      <button className=" hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="video.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myVid
+        </span>
       </button>
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="podcast.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myPodcast</span>
+
+      <button className=" hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="idea.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myIdea
+        </span>
       </button>
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="reuse.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myReuse</span>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="job.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myjob
+        </span>
       </button>
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="food.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">mySlowfood</span>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="podcast.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myPodcast
+        </span>
       </button>
-      <button className="py-1 flex justify-start bg-blue-200 w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
-        <img src="food.svg" alt="" width={30} className="mx-4" />
-        <span className="py-1 flex align-middle text-sm">myCM</span>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="reuse.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myReuse
+        </span>
+      </button>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="food.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          mySlowfood
+        </span>
+      </button>
+      <button className="  hover:bg-[#084fe5] hover:text-white py-1 flex justify-start bg-[#f5f8ff] w-full rounded-t-lg rounded-b-lg h-10 text-black  my-2">
+        <img src="food.svg" alt="" width={20} className="mx-4 my-auto" />
+        <span className="py-1 flex align-middle text-sm font-bold my-auto">
+          myCM
+        </span>
       </button>
     </div>
   );
--- src/Leftcomponent.js
+++ src/Leftcomponent.js
@@ -1,21 +1,44 @@
 function LeftComponent() {
   return (
-    <div className="border-1  border-black mx-4 rounded-xl top-[140px] bg-white h-60 w-[85%] absolute">
-      <img
-        src="blockchainlogo.jpg"
-        alt="logo"
-        width={270}
-        className="rounded-t-xl"
-      />
-      <span className="flex justify-center h-10">
+    <div className=" shadow-2xl border-1  border-black mx-2 rounded-xl top-[140px] bg-white h-[13%] w-[94%] absolute">
+      <img src="blockchainlogo.jpg" alt="logo" className="rounded-t-xl h-29" />
+      <span className="flex justify-center h-5">
         <img
           src="professor.jpg"
           alt="logo"
-          className="rounded-full h-10 w-10 relative bottom-5"
+          className=" rounded-full h-10 w-10 relative bottom-5"
         />
       </span>
-      <p className="text-center font-extrabold	 text-blue-600">Professor</p>
-      <p className="text-center text-sm text-blue-600">Istambul , Turkey</p>
+      <p className=" leading-4	 text-center font-extrabold	 text-blue-600">
+        Professor
+      </p>
+      <p className="leading-4	 text-center text-sm text-blue-600">
+        Istambul , Turkey
+      </p>
+
+      <div className=" leading-5 flex justify-around text-blue-500 text-xs border-t-2 pt-2 mt-2 w-[70%] mx-auto">
+        <div>
+          <span className="font-extrabold">5k</span> Posts
+        </div>
+        <div>
+          <span className="font-extrabold">500</span> videos
+        </div>
+        <div>
+          <span className="font-extrabold">40</span> Books
+        </div>
+      </div>
+
+      <div className=" leading-6 flex justify-around text-blue-500 text-xs  pb-10  w-[90%] mx-auto">
+        <div>
+          <span className="font-extrabold">200K</span> Ideas
+        </div>
+        <div>
+          <span className="font-extrabold">126</span> Introductions
+        </div>
+        <div>
+          <span className="font-extrabold">652</span> Podcast Books
+        </div>
+      </div>
     </div>
   );
 }
